f89fd9aae7cf735222f5c454694b211c71fb4a41
Alexandr Kondratenko <alexandr.kondratenko.exp@gmail.com>
2021-12-19 22:54:47 +0200

    rewrote the component ImageGallery to hooks

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import Api from 'pixabayApi';
@@ -10,109 +10,73 @@ import { List } from './ImageGallery.styled';
 const API = new Api();
 
 const status = {
-  IDLE: 'idle',
   PENDING: 'pending',
   RESOLVED: 'resolved',
 };
-const { IDLE, PENDING, RESOLVED } = status;
-export default class ImageGallery extends Component {
-  state = {
-    images: [],
-    status: PENDING,
-  };
+const { PENDING, RESOLVED } = status;
+export default function ImageGallery({ searchImages, onClick }) {
+  const [collections, setCollections] = useState([]);
+  const [status, setStatus] = useState(PENDING);
+  const refSearchImages = useRef();
 
-  async componentDidMount() {
+  const handleFetchApi = async () => {
     try {
-      const images = await API.fetchImages();
-      this.setState({ images, status: IDLE });
+      API.resetPage();
+
+      const collections = await API.fetchImages();
+      if (!collections.length) {
+        toast.error('images not found');
+      }
+      setCollections(collections);
     } catch {
       toast.error('oops something went wrong');
     }
-  }
-
-  async componentDidUpdate(prevProps, _prevState) {
-    const { searchImages } = this.props;
-
-    if (prevProps.searchImages !== searchImages) {
-      this.setState({
-        status: PENDING,
-      });
-
-      try {
-        API.resetPage();
-        API.query = searchImages;
-        const images = await API.fetchImages();
-
-        if (!images.length) {
-          toast.error('images not found');
-        }
-
-        this.setState({
-          images,
-          status: RESOLVED,
-        });
-      } catch {
-        toast.error('oops something went wrong');
-      }
-    }
-  }
-
-  handleLoadMoreImage = async () => {
-    API.incrementPage();
-    const nextPage = await API.fetchImages();
-    this.setState(({ images }) => ({ images: [...images, ...nextPage] }));
-    this.scrollDown();
   };
 
-  scrollDown() {
+  useEffect(() => {
+    refSearchImages.current = searchImages;
+    setStatus(PENDING);
+    if (refSearchImages !== searchImages) {
+      API.query = searchImages;
+      setTimeout(() => {
+        handleFetchApi();
+        setStatus(RESOLVED);
+      }, 1000);
+    }
+  }, [searchImages]);
+
+  const handleLoadMoreImage = async () => {
+    API.incrementPage();
+    const nextPage = await API.fetchImages();
+    setCollections([...collections, ...nextPage]);
+    scrollDown();
+  };
+
+  const scrollDown = () => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: 'smooth',
     });
+  };
+
+  if (status === PENDING) {
+    return <Loader />;
   }
 
-  render() {
-    const { images, status } = this.state;
-
-    if (status === IDLE) {
-      return (
-        <>
-          <List>
-            {images.map(item => (
-              <ImageGalleryItem
-                key={item.id}
-                params={item}
-                onClick={this.props.onClick}
-              />
-            ))}
-          </List>
-          {images.length > 0 && <Button onClick={this.handleLoadMoreImage} />}
-        </>
-      );
-    }
-
-    if (status === PENDING) {
-      return <Loader />;
-    }
-
-    if (status === RESOLVED) {
-      return (
-        <>
-          <List>
-            {images.map(item => (
-              <ImageGalleryItem
-                key={item.id}
-                params={item}
-                onClick={this.props.onClick}
-              />
-            ))}
-          </List>
-          {images.length > 0 && <Button onClick={this.handleLoadMoreImage} />}
-        </>
-      );
-    }
+  if (status === RESOLVED) {
+    return (
+      <>
+        <List>
+          {collections.map(item => (
+            <ImageGalleryItem key={item.id} params={item} onClick={onClick} />
+          ))}
+        </List>
+        {collections.length > 0 && <Button onClick={handleLoadMoreImage} />}
+      </>
+    );
   }
 }
+
 ImageGallery.propTypes = {
   searchImages: PropTypes.string.isRequired,
   onClick: PropTypes.func.isRequired,
